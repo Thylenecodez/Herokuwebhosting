84e03e1d533901b84be7fcc5fa08cdf2b92f161d
Thylene <slakshay10699@gmail.com>
2021-02-02 00:49:00 +0530

    final website commit

---
+++ Website/Final website/expressWeb/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("", (req, res)=> {
+    res.send("Welcome to Lakshay codez")
+})
+
+app.listen(port , ()=> {
+    console.log('listening to the port at ${port}')
+})
